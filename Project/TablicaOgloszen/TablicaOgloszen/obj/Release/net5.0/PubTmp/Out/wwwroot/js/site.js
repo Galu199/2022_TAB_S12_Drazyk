959ad414874b94fadeab3f9dd80073cd2f2bed5e
Galu199 <Seweryngalu199@gmail.com>
2022-06-20 20:04:34 +0200

    Published Version

---
+++ Project/TablicaOgloszen/TablicaOgloszen/obj/Release/net5.0/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,29 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+//window.onload = function () {
+//    if (window.jQuery) {
+//        // jQuery is loaded  
+//        alert("Yeah!");
+//    } else {
+//        // jQuery is not loaded
+//        alert("Doesn't Work");
+//    }
+//}
+
+$(document).ready(function () {
+    $('.bi').hover(function () {
+        if ($(this).parents('.ratings').length) {
+            $(this).addClass('rating-color');
+            $(this).parent().parent().prevAll().children('a').children('.bi').addClass('rating-color');
+            $(this).parent().parent().nextAll().children('a').children('.bi').removeClass('rating-color');
+        }
+    }, function () {
+        if ($(this).parents('.ratings').length) {
+            $(this).removeClass('rating-color');
+            $(this).parent().parent().prevAll().children('a').children('.bi').removeClass('rating-color');
+        }
+    });
+});
